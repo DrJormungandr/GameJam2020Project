6f7dd0e5ec7f6f6591d83d068744b5fa560e9eba
asedov <avsej93@gmail.com>
2020-05-02 13:42:01 +0700

    Added scripts

--- Assets/Scripts/Models/God.cs
+++ Assets/Scripts/Models/God.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Models
 {
+    [Serializable]
     public class God
     {
        public string Name;
+       public GameObject GodGO;
        public string Description;
        public int Dominance;
     }
